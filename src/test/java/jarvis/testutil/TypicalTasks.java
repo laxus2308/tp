39e2149d6d9757588967881106fa6a27ebd8805c
MokYaoHui <e0725894@u.nus.edu>
2022-10-14 16:17:14 +0800

    Update TypicalTasks

--- src/test/java/jarvis/testutil/TypicalTasks.java
+++ src/test/java/jarvis/testutil/TypicalTasks.java
@@ -1,6 +1,11 @@
 package jarvis.testutil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import jarvis.model.Task;
+import jarvis.model.TaskBook;
 
 /**
  * A utility class containing a list of {@code Task} objects to be used in tests.
@@ -12,5 +17,21 @@ public class TypicalTasks {
     public static final Task QUEST1 = new TaskBuilder().withDesc("Mark quest 1").build();
     public static final Task QUEST2 = new TaskBuilder().withDesc("Mark quest 2").build();
 
+    private TypicalTasks() {} // prevents instantiation
+
+    /**
+     * Returns an {@code TaskBook} with all the typical tasks.
+     */
+    public static TaskBook getTypicalTaskBook() {
+        TaskBook tb = new TaskBook();
+        for (Task task : getTypicalTasks()) {
+            tb.addTask(task);
+        }
+        return tb;
+    }
+
+    public static List<Task> getTypicalTasks() {
+        return new ArrayList<>(Arrays.asList(MISSION1, MISSION2, QUEST1, QUEST2));
+    }
 
 }
